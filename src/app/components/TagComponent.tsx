fca158b71110ffb3227cb338dbbad7fbccedc1c5
Yvan Noah <yvannandy@gmail.com>
2026-02-07 12:19:29 +0300

    Update TagComponent.tsx

--- src/app/components/TagComponent.tsx
+++ src/app/components/TagComponent.tsx
@@ -1,6 +1,6 @@
 export default function TagComponent({ label, color }: any) {
     return (
-        <button className={`bg-${color}-500 hover:bg-${color}-700 text-white font-bold py-2 px-4 rounded-full cursor-pointer`}>
+        <button style={{backgroundColor: `#0b0f17`}} className={`bg-${color}-500 hover:bg-${color}-700 text-white font-bold py-2 px-4 rounded-full cursor-pointer`}>
             {label}
         </button>
     );
