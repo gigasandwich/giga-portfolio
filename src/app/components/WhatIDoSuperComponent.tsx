7ee0aa0c6a96bcd97f574ba42fa5df6d8c606d84
Yvan Noah <yvannandy@gmail.com>
2026-02-07 11:15:09 +0300

    Fix WhatIdoSuperComponent: gap and centering on mobile

--- src/app/components/WhatIDoSuperComponent.tsx
+++ src/app/components/WhatIDoSuperComponent.tsx
@@ -28,8 +28,8 @@ export default function WhatIDoSuperComponent({
     };
 
     return (
-        <div className="sm:flex sm:items-center sm:justify-center">
-            <div className="relative px-16 grid grid-rows-9 md:grid-rows-12 grid-cols-1">
+        <div className="flex items-center justify-center">
+            <div className="relative px-4 sm:px-8 md:px-12 lg:px-16 grid grid-rows-9 md:grid-rows-12 grid-cols-1">
                 <h1 className="text-4xl font-bold sm:row-span-1 mb:row-span-2">{title}</h1>
 
                 <div className="row-span-6">
